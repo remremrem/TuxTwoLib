5b8be7b865d1898f3d2692180c77c525c76642c2
Cat73 <1901803382@qq.com>
2016-06-10 22:52:15 +0800

    Updated to 1.10

--- src/Tux2/TuxTwoLib/TuxTwoInventoryPlayer.java
+++ src/Tux2/TuxTwoLib/TuxTwoInventoryPlayer.java
@@ -2,77 +2,77 @@ package Tux2.TuxTwoLib;
 
 import java.util.HashMap;
 
-import net.minecraft.server.v1_9_R2.PlayerInventory;
-
 import org.bukkit.Bukkit;
-import org.bukkit.craftbukkit.v1_9_R2.inventory.CraftInventoryPlayer;
+import org.bukkit.craftbukkit.v1_10_R1.inventory.CraftInventoryPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import net.minecraft.server.v1_10_R1.PlayerInventory;
+
 public class TuxTwoInventoryPlayer extends CraftInventoryPlayer {
-	
-	public TuxTwoInventoryPlayer(PlayerInventory inventory) {
-		super(inventory);
-	}
-	
-	public TuxTwoInventoryPlayer(CraftInventoryPlayer inventory) {
-		super(inventory.getInventory());
-	}
 
-	@Override
-	public void setArmorContents(ItemStack[] items) {
-		super.setArmorContents(items);
-		InventoryChangeEvent eventcall = new InventoryChangeEvent((Player)getHolder(), 0, true, items);
-		Bukkit.getServer().getPluginManager().callEvent(eventcall);
-	}
+    public TuxTwoInventoryPlayer(final PlayerInventory inventory) {
+        super(inventory);
+    }
 
-	@Override
-	public void setBoots(ItemStack boots) {
-		super.setBoots(boots);
-		InventoryChangeEvent eventcall = new InventoryChangeEvent((Player)getHolder(), 0, true, boots);
-		Bukkit.getServer().getPluginManager().callEvent(eventcall);
-	}
+    public TuxTwoInventoryPlayer(final CraftInventoryPlayer inventory) {
+        super(inventory.getInventory());
+    }
 
-	@Override
-	public void setChestplate(ItemStack chestplate) {
-		super.setChestplate(chestplate);
-		InventoryChangeEvent eventcall = new InventoryChangeEvent((Player)getHolder(), 2, true, chestplate);
-		Bukkit.getServer().getPluginManager().callEvent(eventcall);
-	}
+    @Override
+    public void setArmorContents(final ItemStack[] items) {
+        super.setArmorContents(items);
+        final InventoryChangeEvent eventcall = new InventoryChangeEvent((Player) this.getHolder(), 0, true, items);
+        Bukkit.getServer().getPluginManager().callEvent(eventcall);
+    }
 
-	@Override
-	public void setHelmet(ItemStack helmet) {
-		super.setHelmet(helmet);
-		InventoryChangeEvent eventcall = new InventoryChangeEvent((Player)getHolder(), 3, true, helmet);
-		Bukkit.getServer().getPluginManager().callEvent(eventcall);
-	}
+    @Override
+    public void setBoots(final ItemStack boots) {
+        super.setBoots(boots);
+        final InventoryChangeEvent eventcall = new InventoryChangeEvent((Player) this.getHolder(), 0, true, boots);
+        Bukkit.getServer().getPluginManager().callEvent(eventcall);
+    }
 
-	@Override
-	public void setLeggings(ItemStack leggings) {
-		super.setLeggings(leggings);
-		InventoryChangeEvent eventcall = new InventoryChangeEvent((Player)getHolder(), 1, true, leggings);
-		Bukkit.getServer().getPluginManager().callEvent(eventcall);
-	}
+    @Override
+    public void setChestplate(final ItemStack chestplate) {
+        super.setChestplate(chestplate);
+        final InventoryChangeEvent eventcall = new InventoryChangeEvent((Player) this.getHolder(), 2, true, chestplate);
+        Bukkit.getServer().getPluginManager().callEvent(eventcall);
+    }
 
-	@Override
-	public HashMap<Integer, ItemStack> addItem(ItemStack... items) {
-		InventoryChangeEvent eventcall = new InventoryChangeEvent((Player)getHolder(), items);
-		Bukkit.getServer().getPluginManager().callEvent(eventcall);
-		return super.addItem(items);
-	}
+    @Override
+    public void setHelmet(final ItemStack helmet) {
+        super.setHelmet(helmet);
+        final InventoryChangeEvent eventcall = new InventoryChangeEvent((Player) this.getHolder(), 3, true, helmet);
+        Bukkit.getServer().getPluginManager().callEvent(eventcall);
+    }
 
-	@Override
-	public void setContents(ItemStack[] items) {
-		super.setContents(items);
-		InventoryChangeEvent eventcall = new InventoryChangeEvent((Player)getHolder(), 0, items);
-		Bukkit.getServer().getPluginManager().callEvent(eventcall);
-	}
+    @Override
+    public void setLeggings(final ItemStack leggings) {
+        super.setLeggings(leggings);
+        final InventoryChangeEvent eventcall = new InventoryChangeEvent((Player) this.getHolder(), 1, true, leggings);
+        Bukkit.getServer().getPluginManager().callEvent(eventcall);
+    }
 
-	@Override
-	public void setItem(int index, ItemStack item) {
-		super.setItem(index, item);
-		InventoryChangeEvent eventcall = new InventoryChangeEvent((Player)getHolder(), index, item);
-		Bukkit.getServer().getPluginManager().callEvent(eventcall);
-	}
+    @Override
+    public HashMap<Integer, ItemStack> addItem(final ItemStack... items) {
+        final InventoryChangeEvent eventcall = new InventoryChangeEvent((Player) this.getHolder(), items);
+        Bukkit.getServer().getPluginManager().callEvent(eventcall);
+        return super.addItem(items);
+    }
+
+    @Override
+    public void setContents(final ItemStack[] items) {
+        super.setContents(items);
+        final InventoryChangeEvent eventcall = new InventoryChangeEvent((Player) this.getHolder(), 0, items);
+        Bukkit.getServer().getPluginManager().callEvent(eventcall);
+    }
+
+    @Override
+    public void setItem(final int index, final ItemStack item) {
+        super.setItem(index, item);
+        final InventoryChangeEvent eventcall = new InventoryChangeEvent((Player) this.getHolder(), index, item);
+        Bukkit.getServer().getPluginManager().callEvent(eventcall);
+    }
 
 }
